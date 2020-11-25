0442d7c37fb850b5c4c441cecad9cd1ec1b93346
Arselena <efedorovyh@yandex.ru>
2020-11-25 23:21:19 +0500

    10. Экономим тонер

---
+++ Level_0_10.py
@@ -0,0 +1,28 @@
+from openpyxl import Workbook, load_workbook
+
+def PrintingCosts(Line):
+    try:
+        assert type(Line) is str
+
+        ARR = list(Line)    
+        WB = load_workbook(filename = r'.\higher-school\Lib\ConsTon.xlsx')  # Открываем книгу
+        SH = WB.active  # Переменная sheet = активному листу в книге wb 
+        
+        row_max = SH.max_row  # Получаем количество столбцов 
+        column_max = SH.max_column  # Получаем количество строк
+
+        sum = 0  # итоговая сумма
+        for i in range(len(ARR)):  # i-ый эл-т строки
+            fl = False  # флаг для отслеживания изменения суммы
+            for j in range(1, row_max + 1):  # j-ая строка в файле
+                if ARR[i] == SH.cell(row=j, column=1).value:
+                    sum += SH.cell(row=j, column=2).value
+                    fl = True
+                    break
+            if fl == False:
+                sum += 23
+        
+        return sum
+    
+    except AssertionError:
+        pass
