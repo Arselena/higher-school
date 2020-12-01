0f7b87fa7e0983e54e0b44dbb2f3cd12c623c659
Arselena <efedorovyh@yandex.ru>
2020-12-01 14:20:00 +0500

    15. Танковый раш. Разбиене строки на подстоку

--- Level_0_15.py
+++ Level_0_15.py
@@ -1,6 +1,9 @@
 # TankRush возвращает true, если S2(str) содержится S1(str) 
 def TankRush(H1, W1, S1, H2, W2, S2):
     
+    S1 = S1.split()  # разбивает строку на подстроку по пробелу
+    S2 = S2.split()
+
     for i in range(H2):
         flag = False
         for j in range(H1):
