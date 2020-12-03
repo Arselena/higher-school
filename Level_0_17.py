7a69a9bdb230eb4597030212d9c20b8098aa35c6
Arselena <efedorovyh@yandex.ru>
2020-12-03 18:38:28 +0500

    17. Машинное распознавание паттернов

---
+++ Level_0_17.py
@@ -0,0 +1,15 @@
+def LineAnalysis(line:str):
+    a = ''
+    if line == '' or line == '*' or line == '**' or line == '*.*' or line == '***':
+        return True
+    
+    N = len(line)
+    s = line[1:N-1].split('*') # Убираем крайние звездочки и разделяем на подстроки по '*'
+    for i in range(len(s)-1):
+        if s[i] == s[i+1]:
+            flag = True
+        else: flag = False
+    return flag
+
+# print(LineAnalysis('*..*..*..*..*..*..*'))
+print(LineAnalysis('**..**..**..***'))
