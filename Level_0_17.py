2e0f09538fc473ab07b13b36768a190fe6ee303e
Arselena <efedorovyh@yandex.ru>
2020-12-04 07:05:31 +0500

    17. Машинное распознавание паттернов

--- Level_0_17.py
+++ Level_0_17.py
@@ -1,14 +1,11 @@
 def LineAnalysis(line:str):
-    if len(line) % 2 == 0:
-        n1 = n2 = len(line) // 2
-    else:
-        n1 = len(line) // 2
-        n2 = len(line) // 2 + 1
-
-    line1 = line[:n1]
-    line2 = line[n2:]
-    line2 = line2[::-1]
-    if line1 == line2:
-        return True
-    else: 
-        return False
+    n = len(line)
+    line = line[1:n-1]
+    line = line.split('*')
+    flag = True
+    if len(line) >= 2:
+        for i in range(len(line) - 1):
+            if line[i] != line[i + 1]:
+                flag = False
+                break
+    return flag
