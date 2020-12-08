882a044c44e1cd643bd62f20e7fd9b23e9a28d3b
Arselena <efedorovyh@yandex.ru>
2020-12-09 02:33:29 +0500

    19. Add группировку по кол-ву товара

--- Level_0_19.py
+++ Level_0_19.py
@@ -1,13 +1,24 @@
 def ShopOLAP(N:int, items:str):
 
-    def sort_quantity(s): # Сортировка по кол-ву
-        return s[1]
+    def sum(s_new):  # Складывает одинаковые позиции 
+        i = 0
+        while i in range(len(s_new) - 1):  
+            while s_new[i][0] == s_new[i+1][0]: 
+                a = int(s_new[i][1]) + int(s_new[i + 1][1])
+                s_new[i][1] = str(a)
+                del s_new[i + 1]
+            i += 1
+        return s_new
+
+    def sort_quantity(s): # Ключ для сортировки по кол-ву, кол-во преобразовать в int!
+        return int(s[1])
 
     s_new = []
     for i in items: # Преобразовываем в список
         s_new.append(i.split())
     
     s_new.sort() # Сортируем по алфавиту
+    s_new = sum(s_new) # Складываем одинаковые позиции 
     s_new.sort(key=sort_quantity, reverse=True) # Сортируем по кол-ву в порядке убывания
     
     s_new_str = []  # Преобразовываем в строку
