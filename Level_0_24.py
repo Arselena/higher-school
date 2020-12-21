758065168d1eae9abbfb2523aa977a19e57443c8
Arselena <efedorovyh@yandex.ru>
2020-12-21 17:24:59 +0500

    24. Матрица: Вращение. Del dif rotate

--- Level_0_24.py
+++ Level_0_24.py
@@ -1,16 +1,5 @@
 def MatrixTurn(Matrix:str, M:int, N:int, T:int):
 
-    def rotate(Matrix, i1, i2, j1, j2, Et): # i1, i2, j1, j2 - края 'кольца', Et = Matrix_copy
-        if i2+1 // 2 >= len(Matrix) // 2:
-            for j in range(j1, j2):
-                Matrix[i1][j+1] = Et[i1][j]  # Поворот верхнего края
-                Matrix[i2][j] = Et[i2][j+1]  # Поворот нижнего края
-            for i in range(i1, i2):
-                Matrix[i][j1] = Et[i+1][j1]  # Поворот левого края
-                Matrix[i+1][j2] = Et[i][j2]  # Поворот правого края
-            rotate(Matrix, i1+1, i2-1, j1+1, j2-1, Et)  # Сужаем кольцо матрицы и снова поворачиваем
-        return Matrix
-    
     Matrix = list(Matrix)
     for i in range(M):
         Matrix[i] = list(Matrix[i])
@@ -19,7 +8,21 @@ def MatrixTurn(Matrix:str, M:int, N:int, T:int):
     for i in range(M):
         Matrix_copy[i] = list(Matrix[i])
 
-    rotate(Matrix, 0, (M-1), 0, (N-1), Matrix_copy)
+    i1 = 0
+    i2 = M-1
+    j1 = 0
+    j2 = N-1
+    while i2+1 // 2 >= len(Matrix) // 2:
+        for j in range(j1, j2):
+            Matrix[i1][j+1] = Matrix_copy[i1][j]  # Поворот верхнего края
+            Matrix[i2][j] = Matrix_copy[i2][j+1]  # Поворот нижнего края
+        for i in range(i1, i2):
+            Matrix[i][j1] = Matrix_copy[i+1][j1]  # Поворот левого края
+            Matrix[i+1][j2] = Matrix_copy[i][j2]  # Поворот правого края
+        i1 += 1
+        i2 -= 1
+        j1 += 1
+        j2 -= 1
     
     for i in range(len(Matrix)):
         Matrix[i] = ''.join(Matrix[i])
