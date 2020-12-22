67d13da89db1478b9e3a589aaae41eda7622f01a
Arselena <efedorovyh@yandex.ru>
2020-12-22 21:48:22 +0500

    25. Чубакка и гравитационные колодцы

--- Level_0_25.py
+++ Level_0_25.py
@@ -1,17 +1,16 @@
 def TransformTransform(A:int, N:int):
-    def tratsform(A):
+    def S(A):
         B = []
         for i in range(len(A)):
             for j in range(len(A)-i):
                 k = i + j
-                a = A[j:k]
+                a = A[j:(k+1)]
                 if a != []:
                     a_max = max(a)
                     B.append(a_max)
         return B
     
-    b = tratsform(tratsform(A))
-    
+    b = S(S(A))
     rez = False
     if sum(b) % 2 == 0:
         rez = True
