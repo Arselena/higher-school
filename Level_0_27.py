4738f4636a20c6f57dc18f9759cee459012e266e
Arselena <efedorovyh@yandex.ru>
2020-12-24 22:15:01 +0500

    27. Тренируем сборную России по футболу

---
+++ Level_0_27.py
@@ -0,0 +1,22 @@
+def Football(F, N:int):
+    
+    F_sort = sorted(F)
+    F_rev = reversed(F)    
+    index = []
+
+    if F == F_sort:  # Если массив изначально отсортирован
+        return True
+    
+    for i in range(len(F)):  # Запишем индексы значений не на своих местах
+        if F[i] != F_sort[i]:
+            index.append(i)
+
+    if len(index) == 2:  # если не совпали два значения
+        return True
+    elif index not in list(range(index[0], (index[0] + len(index) + 1))): # если не совпадения идут не по порядку
+        return False
+    
+    for i in index:  # усли размернутый массив совпадает с отсортированным в диапазоне индексов не на своем месте
+        if F_rev[index[i]] != F_sort[index[i]]:
+            return False
+    return True
